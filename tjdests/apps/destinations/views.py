91b334411071d3aa18dc26b6b67b5d81833bc451
Leah Zhang <LeahZhang1595995@gmail.com>
2025-01-09 19:18:58 -0500

    fixed

--- tjdests/apps/destinations/views.py
+++ tjdests/apps/destinations/views.py
@@ -9,7 +9,8 @@ from django.views.generic import ListView
 
 from ..authentication.models import User
 from .models import College, Decision, TestScore
-
+from .forms import FilterForm
+from django.shortcuts import render
 
 class StudentDestinationListView(
     LoginRequiredMixin, UserPassesTestMixin, ListView
@@ -20,8 +21,9 @@ class StudentDestinationListView(
     def get_queryset(self):
         # Superusers can use the "all" GET parameter to see all data
         print(self.request.GET) 
-
-        # Getting Queryset
+        form = FilterForm(self.request.GET or None)
+        
+        # Getting Queryset  
         if self.request.GET.get("all", None) is not None:
             if self.request.user.is_superuser and self.request.user.is_staff:
                 queryset = User.objects.all()
@@ -33,103 +35,73 @@ class StudentDestinationListView(
         queryset = queryset.filter(is_senior=True).order_by(
             "last_name", "preferred_name"
         )
-        
 
-        #College 
-        college_id: Optional[str] = self.request.GET.get("college", None)
-        
-            
-        if college_id is not None and college_id != '':
-            if not college_id.isdigit():
-                raise Http404()
-            get_object_or_404(College, id=college_id)
-            queryset = queryset.filter(decision__college__id=college_id)
-            # Decision Type 
-            decision_type: Optional[str] = self.request.GET.get("decision", None)
-            print(decision_type)
-            if decision_type is not None and decision_type != '':
-                queryset = queryset.filter(decision__decision_type=decision_type)
-            
-            admission_type: Optional[list] = self.request.GET.getlist("admission")
-            if admission_type:  # Check if any admission types were selected
-                queryset = queryset.filter(decision__admission_status__in=admission_type)
+        #  Populate the form dynamically
+        colleges = College.objects.all()
+        decisions = Decision.DECISION_TYPE_CHOICES
+        admissions = Decision.ADMIT_TYPE_CHOICES
+        form = FilterForm(self.request.GET or None)
+        form.fields['college'].choices = [("", "Select a College")] + [(college.id, f"{college.name} - {college.location}") for college in colleges]
+        form.fields['decision'].choices = decisions
+        form.fields['admission'].choices = admissions
 
-        # GPA   
-        gpa_max: Optional[str] = self.request.GET.get("gpa_max", '')
-        gpa_min: Optional[str] = self.request.GET.get("gpa_min", '')
+        # Filtering 
+        if form.is_valid():
+            data = form.cleaned_data
+            if data['gpa_min']:
+                queryset = queryset.filter(GPA__gte=data['gpa_min'])
+            if data['gpa_max']:
+                queryset = queryset.filter(GPA__lte=data['gpa_max'])
+            if data['college']:
+                queryset = queryset.filter(decision__college__id=data['college'])
+                if data['decision']:
+                    decisions = self.request.GET.getlist('decision')
+                    print(decisions)
+                    queryset = queryset.filter(decision__decision_type__in=decisions)
+                if data['admission']:
+                    admissions = self.request.GET.getlist('admission')
+                    queryset = queryset.filter(decision__admission_status__in=admissions).distinct()
+            # SAT Filters
+            sat_min = self.request.GET.get("sat_min", '').strip()
+            sat_max = self.request.GET.get("sat_max", '').strip()
+            if sat_max != '' and sat_min == '':
+                sat_min = "0"
+            if sat_min != '' and sat_max == '':
+                sat_max = "1600"
+            if sat_min != '' or sat_max != '':
+                if int(sat_min) > int(sat_max):
+                    raise Http404("Invalid SAT score range")
+                sat_min = int(sat_min)
+                sat_max = int(sat_max)
 
-        if gpa_max!='' and gpa_min=='': gpa_min="0"
-        if gpa_min!='' and gpa_max=='': gpa_max="5"
-        
-        if gpa_max!='' and gpa_min!='':
-            
-            if gpa_min>gpa_max:
-                raise Http404()
-            
-            gpa_min = float(gpa_min) 
-            gpa_max = float(gpa_max) 
-            
-            if gpa_min:
-                queryset = queryset.filter(GPA__gte=gpa_min)  # GPA >= gpa_min
+                sat_filter = Q(testscore__exam_type="SAT_TOTAL")
+                if sat_min:
+                    sat_filter &= Q(testscore__exam_score__gte=sat_min)
+                if sat_max:
+                    sat_filter &= Q(testscore__exam_score__lte=sat_max)
+                queryset = queryset.filter(sat_filter)
 
-            if gpa_max:
-                queryset = queryset.filter(GPA__lte=gpa_max)  # GPA <= gpa_max
+            # ACT Filters
+            act_min = self.request.GET.get("act_min", '').strip()
+            act_max = self.request.GET.get("act_max", '').strip()
+            if act_max != '' and act_min == '':
+                act_min = "0"
+            if act_min != '' and act_max == '':
+                act_max = "36"
+            if act_min != '' or act_max != '':
+                if int(act_min) > int(act_max):
+                    raise Http404("Invalid ACT score range")
+                act_min = int(act_min)
+                act_max = int(act_max)
 
-         # Test Types and Scores Filter
-        test_types: Optional[list] = self.request.GET.getlist("test_score", [])
-        if test_types:
-            queryset = queryset.filter(testscore__exam_type__in=test_types)
+                act_filter = Q(testscore__exam_type="ACT_COMP")
+                if act_min:
+                    act_filter &= Q(testscore__exam_score__gte=act_min)
+                if act_max:
+                    act_filter &= Q(testscore__exam_score__lte=act_max)
+                queryset = queryset.filter(act_filter)
 
-        # SAT Filter
-        sat_min: Optional[str] = self.request.GET.get("sat_min", '')
-        sat_max: Optional[str] = self.request.GET.get("sat_max", '')
-        if sat_max != '' and sat_min == '': sat_min = "0"
-        if sat_min != '' and sat_max == '': sat_max = "1600"
-        if sat_min != '' or sat_max != '':
-            if sat_min > sat_max:
-                raise Http404("Invalid SAT score range")
-            
-            sat_min = int(sat_min)  # Default min = 0
-            sat_max = int(sat_max)  # Default max = 1600
-
-            sat_filter = Q(testscore__exam_type="SAT_TOTAL")
-            if sat_min:
-                sat_filter &= Q(testscore__exam_score__gte=sat_min)
-
-            if sat_max:
-                sat_filter &= Q(testscore__exam_score__lte=sat_max)
-
-        # ACT Filter
-        act_min: Optional[str] = self.request.GET.get("act_min", '')
-        act_max: Optional[str] = self.request.GET.get("act_max", '')
-        if act_max != '' and act_min == '': act_min = "0"
-        if act_min != '' and act_max == '': act_max = "36"
-        if act_min != '' or act_max != '':
-            if act_min > act_max:
-                raise Http404("Invalid ACT score range")
-            
-            act_min = int(act_min)  # Default min = 0
-            act_max = int(act_max)  # Default max = 36
-
-            act_filter = Q(testscore__exam_type="ACT_COMP")
-            if act_min:
-                act_filter &= Q(testscore__exam_score__gte=act_min)
-
-            if act_max:
-                act_filter &= Q(testscore__exam_score__lte=act_max)
-
-        final_filter = Q()
-
-        if sat_min != '' or sat_max != '':
-            final_filter |= sat_filter  # Apply SAT filter if SAT is provided
-
-        if act_min != '' or act_max != '':
-            final_filter |= act_filter  # Apply ACT filter if ACT is provided
-
-        queryset = queryset.filter(final_filter)
-        queryset = queryset.distinct()
-        
-        print("set", queryset)
+        print(queryset)
         return queryset
 
     def get_context_data(
@@ -137,63 +109,41 @@ class StudentDestinationListView(
     ):  # pylint: disable=unused-argument
         context = super().get_context_data(**kwargs)
 
-       
-        context['TEST_TYPES'] = TestScore.TEST_TYPES
+    
+        # Fetch dynamic choices
+        colleges = College.objects.all()
+        decisions = Decision.DECISION_TYPE_CHOICES
+        admissions = Decision.ADMIT_TYPE_CHOICES
+
+        # Populate the form dynamically
+        form = FilterForm(self.request.GET or None)
+        form.fields['college'].choices = [("", "Select a College")] + [(college.id, f"{college.name} - {college.location}") for college in colleges]
+        form.fields['decision'].choices = decisions
+        form.fields['admission'].choices = admissions
+
+        context['form'] = form
+        college_id = self.request.GET.get("college", None)
+        context["college"] = (
+            get_object_or_404(College, id=college_id) if college_id else None
+        )
+
+        context["DECISION"] = ', '.join(self.request.GET.getlist("decision", []))
+        context["ADMISSION"] = ', '.join(self.request.GET.getlist("admission", []))
+        context["search_query"] = self.request.GET.get("q", None)
+        
+        context["GPA_MIN"] = self.request.GET.get("gpa_min", "0") or "0"
+        context["GPA_MAX"] = self.request.GET.get("gpa_max", "5") or "5"
+        context["SAT_MIN"] = self.request.GET.get("sat_min", "0") or "0"
+        context["SAT_MAX"] = self.request.GET.get("sat_max", "1600") or "1600"
+        context["ACT_MIN"] = self.request.GET.get("act_min", "0") or "0"
+        context["ACT_MAX"] = self.request.GET.get("act_max", "36") or "36"
+ 
         context['COLLEGES'] = College.objects.all()
         context['DECISIONS'] = Decision.DECISION_TYPE_CHOICES
         context['ADMISSIONS'] = Decision.ADMIT_TYPE_CHOICES
-        
-        # College
-        college_id: Optional[str] = self.request.GET.get("college", None)
-        if college_id is not None and college_id != '':
-            context["college"] = get_object_or_404(College, id=college_id)
-            # Decision Type
-            decision_type: Optional[str] = self.request.GET.get("decision", None)
-            if decision_type is not None and decision_type != '':
-                context["DECISION"] = decision_type
-            admission_type: Optional[list] = self.request.GET.getlist("admission")  # Get all selected admissions
-            if admission_type:  # If there's at least one admission type selected
-                context["ADMISSION"] = ', '.join(admission_type)    
-
-        # GPA
-        gpa_min: Optional[str] = self.request.GET.get("gpa_min", '')
-        gpa_max: Optional[str] = self.request.GET.get("gpa_max", '')
-        
-        if  gpa_min=='': gpa_min="0"
-        if gpa_max=='': gpa_max="5"
-
-        context["GPA_MIN"] = gpa_min
-        context["GPA_MAX"] = gpa_max
-        
-        # SAT
-        sat_min: Optional[str] = self.request.GET.get("sat_min", "0")
-        sat_max: Optional[str] = self.request.GET.get("sat_max", "1600")
-        if  sat_min=='': sat_min="0"
-        if sat_max=='': sat_max="1600"
-        print(sat_min, sat_max)
-        context["SAT_MIN"] = sat_min
-        context["SAT_MAX"] = sat_max
-
-        # ACT
-        act_min: Optional[str] = self.request.GET.get("act_min", "0")
-        act_max: Optional[str] = self.request.GET.get("act_max", "36")
-        if  act_min=='': act_min="0"
-        if act_max=='': act_max="36"
-        print(act_min, act_max)
-        context["ACT_MIN"] = act_min
-        context["ACT_MAX"] = act_max
-
-
-        # Test Types
-        test_types: Optional[list] = self.request.GET.getlist("test_score", '')    
-        print(test_types)
-        context["SELECTED_TEST_SCORES"] = test_types
-        
-        search_query = self.request.GET.get("q", None)
-        if search_query is not None:
-            context["search_query"] = search_query
-
         return context
+    
+    
 
     def test_func(self):
         assert self.request.user.is_authenticated
